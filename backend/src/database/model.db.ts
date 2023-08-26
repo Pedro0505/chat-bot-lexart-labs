166e5552ed3cac041cbf0f3b355dd2e6e64daae7
pedro0505 <pedrohrds05@hotmail.com>
2023-08-26 07:16:39 -0300

    feat: adding user model

---
+++ backend/src/database/model.db.ts
@@ -0,0 +1,24 @@
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ModelNames } from 'src/constants/ModelNames';
+import { UserSchema } from './models/user.model';
+
+export const modelForFeature = MongooseModule.forFeature([
+  {
+    name: ModelNames.USER,
+    schema: UserSchema,
+  },
+]);
+
+export const modelForRoot = MongooseModule.forRootAsync({
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: async (config: ConfigService) => {
+    const nodeEnv = config.get<string>('NODE_ENV');
+    const dbName = nodeEnv === 'TEST' ? 'DATABASE_URL_TEST' : 'DATABASE_URL';
+    const uri = config.get<string>(dbName);
+    return {
+      uri,
+    };
+  },
+});
