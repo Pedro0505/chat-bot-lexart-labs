a7dee2c622d887a5ffa94b1c92331eded35453fe
pedro0505 <pedrohrds05@hotmail.com>
2023-08-26 12:10:08 -0300

    feat: implemented user controller

---
+++ backend/src/constants/ApiRoutes.ts
@@ -0,0 +1,9 @@
+export enum ApiRoutes {
+  USER = '/user',
+  REGISTER = '/register',
+  LOGIN = '/login',
+  HISTORY = '/history',
+  USER_REGISTER = '/user/register',
+  USER_LOGIN = '/user/login',
+  USER_HISTORY = '/user/history',
+}
--- backend/src/shared/entities/user/user.controller.ts
+++ backend/src/shared/entities/user/user.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/UserCreate.dto';
+import { LoginUserDto } from './dto/UserLogin.dto';
+import { ApiRoutes } from 'src/constants/ApiRoutes';
+import { ConversationCreateDto } from './dto/ConversationCreate.dto';
 
-@Controller()
-export class UserController {}
+@Controller(ApiRoutes.USER)
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post(ApiRoutes.REGISTER)
+  public async register(@Body() body: CreateUserDto) {
+    return this.userService.register(body);
+  }
+
+  @Post(ApiRoutes.LOGIN)
+  public async login(@Body() body: LoginUserDto) {
+    return this.userService.login(body);
+  }
+
+  @Post(ApiRoutes.HISTORY)
+  public async registerHistory(@Body() body: ConversationCreateDto) {
+    return this.userService.registerHistory(body);
+  }
+
+  @Get(ApiRoutes.HISTORY)
+  public async getHistory() {
+    return this.userService.getHistory();
+  }
+}
