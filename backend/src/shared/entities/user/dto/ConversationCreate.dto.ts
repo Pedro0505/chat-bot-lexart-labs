8111fec9630e0ba919b05dce97fe44d902399bd1
pedro0505 <pedrohrds05@hotmail.com>
2023-08-26 16:42:00 -0300

    feat: adding body validations

--- backend/src/shared/entities/user/dto/ConversationCreate.dto.ts
+++ backend/src/shared/entities/user/dto/ConversationCreate.dto.ts
@@ -1,3 +1,8 @@
+import { IsNotEmpty, IsDefined, IsDate } from 'class-validator';
+
 export class ConversationCreateDto {
+  @IsNotEmpty({ message: 'A data de usuário não pode ser vazia' })
+  @IsDefined({ message: 'A data de usuário não pode ser vazia' })
+  @IsDate({ message: 'A data de usuário tem que ser uma data valida' })
   public waxing_time: Date;
 }
