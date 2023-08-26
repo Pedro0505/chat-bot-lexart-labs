7309fb05bf77f0906126a6b62d1779ee2875de0e
pedro0505 <pedrohrds05@hotmail.com>
2023-08-26 12:10:17 -0300

    feat: implemented user module

--- backend/src/shared/entities/user/user.module.ts
+++ backend/src/shared/entities/user/user.module.ts
@@ -3,9 +3,11 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { UserRepository } from './user.repository';
 import { modelForFeature, modelForRoot } from 'src/database/model.db';
+import { AuthModule } from '../auth/auth.module';
+import { UtilsModule } from 'src/shared/utils/utils.module';
 
 @Module({
-  imports: [modelForFeature, modelForRoot],
+  imports: [modelForFeature, modelForRoot, AuthModule, UtilsModule],
   providers: [UserService, UserRepository],
   controllers: [UserController],
 })
